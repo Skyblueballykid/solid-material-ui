bb5c7e250b4b1a7e7ff613271063b419f9e6dd73
David Zarandi <david.zarandi@pm.me>
2022-03-10 17:29:43 +0100

    proof of concept css-in-js with emotion and theme

--- docs/app.tsx
+++ docs/app.tsx
@@ -1,11 +1,28 @@
 import { Component } from 'solid-js'
+import { css } from '@emotion/css'
 import { useMediaQuery } from '../src/hooks/useMediaQuery'
+import { Theme } from '../src/theme'
+import { useTheme } from '../src/context/theme'
+
+const getStyle = (theme: Theme): string => css({
+  paddingTop: 32,
+  [`@media ${theme.breakpoints.only('sm')}`]: {
+    paddingTop: 64
+  }
+})
 
 export const App: Component = () => {
   return <div><Element /></div>
 }
 
 const Element: Component = () => {
-  const isMobile = useMediaQuery(() => '(max-width:600px)')
-  return <div>{isMobile() ? 'mobile' : 'not mobile'}</div>
+  const theme = useTheme()
+  console.log(theme.breakpoints.only('sm'))
+  const isMobile = useMediaQuery((theme) => theme.breakpoints.only('sm'))
+  return (
+    <div>
+      {isMobile() ? 'mobile' : 'not mobile'}
+      <div className={getStyle(theme)}>Yo</div>
+    </div>
+  )
 }
